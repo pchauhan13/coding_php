a35067ef4b7d4de11d64b10c80d17df5086feae7
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-29 16:51:53 +0530

    define and use variables

---
+++ program_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 2</title>
+</head>
+<body>
+    <?php
+
+        $characterName = "George";
+        $characterAge = 35;
+
+        echo "There was once a man named $characterName <br>";
+        echo "He was $characterAge years old <br>";
+        echo "He really liked the name $characterName <br>";
+        echo "But didn't like being $characterAge <br>";
+    ?>
+</body>
+</html>
