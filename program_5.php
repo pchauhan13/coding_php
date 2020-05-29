203be41118ca10d2a7da01583625366eca9c9cc3
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-29 20:38:07 +0530

    program to work with numbers

---
+++ program_5.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 5</title>
+</head>
+<body>
+    <?php
+        echo 40, "<br>";
+        echo -40, "<br>";
+        echo 50.4343, "<br>";
+        echo 1 + 2, "<br>";
+        echo 1 - 2, "<br>";
+        echo 1 * 2, "<br>";
+        echo 1 / 2, "<br>";
+        echo 1 % 2, "<br>";
+
+        $num = 10;
+        echo $num, "<br>";
+        $num++;
+        echo $num, "<br>";
+        $num += 10;
+        echo $num, "<br>";
+        echo abs(-100), "<br>";
+        echo pow(2, 3), "<br>";
+        echo sqrt(144), "<br>";
+        echo round(4.343), "<br>";
+        echo floor(4.334), "<br>";
+        echo ceil(4.334), "<br>";
+    ?>
+</body>
+</html>
