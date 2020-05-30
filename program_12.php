5d8c26ab2a9d97e5b4c99e2eb8182ef65ad7b082
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-30 18:34:51 +0530

    program to use return statements in function

---
+++ program_12.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 12</title>
+</head>
+<body>
+    <?php
+        function cube ($num)
+        {
+            return $num * $num * $num;
+        }
+
+        echo cube(4), "<br>";
+    ?>
+</body>
+</html>
