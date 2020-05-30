84f0a97e60e6de112b17ae1279b11f7613bfb3ba
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-30 18:27:01 +0530

    program to define and use associative arrays

---
+++ program_10.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 10</title>
+</head>
+<body>
+    <?php
+        $grades = array("Jim" => "A+", "Pam" => "B-");
+        echo $grades, "<br>";
+        echo $grades["Jim"], "<br>";
+        echo $grades[0], "<br>";
+    ?>
+</body>
+</html>
