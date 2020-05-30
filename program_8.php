a7e8c0758a1e73f160bfd4ceca0af4b19e390447
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-30 10:55:23 +0530

    program to define and use arrays

---
+++ program_8.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 8</title>
+</head>
+<body>
+    <?php
+        $friends = array("Kevin", "Karen", "Oscar", "Jim");
+        echo $friends, "<br>";
+        echo $friends[0], "<br>";
+    ?>
+</body>
+</html>
