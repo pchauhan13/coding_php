de26428852c8702f4e9e66d45e1801ab25fead35
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-03 21:29:35 +0530

    program to define and instantiate a class

---
+++ program_21.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 21</title>
+</head>
+<body>
+    <?php
+        class Book
+        {
+            var $title;
+            var $author;
+            var $pages;
+        }
+
+        $book = new Book;
+        $book->title = "program 21";
+        $book->author = "prashant";
+        $book->pages = 3423;
+
+        // echo $book;
+        echo $book->author, "<br>";
+    ?>
+</body>
+</html>
