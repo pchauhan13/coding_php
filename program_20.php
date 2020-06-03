4a1a6a6efd8c314c58bfe7a7280bda5c148b148b
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-03 21:23:47 +0530

    sharing variales and functions among php files

---
+++ program_20.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 20</title>
+</head>
+<body>
+    <?php
+        $title = "program 20";
+        $author = "prashant";
+        $wordCount = "20";
+        include "include.php";
+        sayHi();
+    ?>
+</body>
+</html>
