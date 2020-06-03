e7d289d60c64bfea03af5715e05be8e137880aa7
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-03 12:28:50 +0530

    program to use function with if..else statements

---
+++ program_14.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Program 14</title>
+</head>
+<body>
+    <?php
+        function getMax($num1, $num2)
+        {
+            if ($num1 > $num2)
+            {
+                return $num1;
+            }
+            else
+            {
+                $num2;
+            }
+        }
+
+        echo getMax(10, 8), "<br>";
+    ?>
+</body>
+</html>
